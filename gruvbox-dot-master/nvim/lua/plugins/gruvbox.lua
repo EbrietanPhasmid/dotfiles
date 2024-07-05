229adb4a7629fad307988ef5122cc5f5d677e24b
EbrietanPhasmid <zawadiak.mrjz@gmail.com>
2024-07-05 20:48:58 +0200

    kitty and nvim config added!

---
+++ gruvbox-dot-master/nvim/lua/plugins/gruvbox.lua
@@ -0,0 +1,15 @@
+return {
+  "ellisonleao/gruvbox.nvim",
+    name = "gruvbox.nvim",
+    opts = {
+      italic = {
+        strings = true,
+        comments = true,
+        folds = true,
+        operations = false,
+      },
+    overrides = {
+      DashboardHeader = { link = "#faf2cc"  }
+    },
+  }
+}
